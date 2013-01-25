8fea266d836b07cd5029745a9cd304608e0074be
Sean McLellan <sean.mclellan@sixconcepts.com>
2013-01-25 16:55:37 +0000

    Justache fix to support non-escaping.

--- Barista.Core/Justache/VariableReference.cs
+++ Barista.Core/Justache/VariableReference.cs
@@ -7,10 +7,10 @@ namespace Barista.Justache
 
   public class VariableReference : Part
   {
-    private static readonly Regex NotEscapedRegex = new Regex(@"^\{\s*(.+?)\s*\}$");
-    private static readonly Regex FormattingRegex = new Regex(@"^(.+?):\((?<Format>.*)\)$");
+    private static readonly Regex NotEscapedRegex = new Regex(@"^(?<LCB>{)?((?<NotEscaped>&)\s*?)?(?<Path>.+?)(?<TCB>})?$", RegexOptions.Compiled);
+    private static readonly Regex FormattingRegex = new Regex(@"^(?<LCB>{)?((?<NotEscaped>&)\s*?)?(?<Path>.+?):\((?<Format>.*)\)(?<TCB>})?$", RegexOptions.Compiled);
     private readonly string m_path;
-    private readonly bool m_escaped;
+    private readonly bool m_escaped = true;
     private readonly string m_formatString;
 
     public VariableReference(string path)
@@ -22,56 +22,65 @@ namespace Barista.Justache
 
       m_path = path;
 
-      var match = NotEscapedRegex.Match(path);
-      m_escaped = !match.Success;
-
-      if (match.Success)
+      if (!FormattingRegex.IsMatch(path))
       {
-        m_path = match.Groups[1].Value;
+        if (!NotEscapedRegex.IsMatch(path))
+          return;
+
+        var notEscapedMatch = NotEscapedRegex.Match(path);
+        if (notEscapedMatch.Groups["NotEscaped"].Success)
+          m_escaped = false;
+        else if (notEscapedMatch.Groups["LCB"].Success && notEscapedMatch.Groups["TCB"].Success)
+          m_escaped = false;
+        
+        m_path = notEscapedMatch.Groups["Path"].Value;
+        return;
       }
 
-      if (FormattingRegex.IsMatch(path))
-      {
-        match = FormattingRegex.Match(path);
-        m_formatString = match.Groups["Format"].Value;
-        m_path = match.Groups[1].Value;
-      }
+      var match = FormattingRegex.Match(path);
+      if (match.Groups["NotEscaped"].Success)
+        m_escaped = false;
+      else if (match.Groups["LCB"].Success && match.Groups["TCB"].Success)
+        m_escaped = false;
+
+      m_formatString = match.Groups["Format"].Value;
+      m_path = match.Groups["Path"].Value;
     }
 
     public string Path { get { return m_path; } }
 
     public override void Render(RenderContext context)
     {
-      object value = context.GetValue(m_path);
+      var value = context.GetValue(m_path);
 
-      if (value != null)
+      if (value == null)
+        return;
+
+      if (value is DateInstance)
       {
-        if (value is DateInstance)
-        {
-          var dt = value as DateInstance;
+        var dt = value as DateInstance;
 
-          if (dt.ToString() == "Invalid Date")
-            return;
+        if (dt.ToString() == "Invalid Date")
+          return;
 
-          value = DateTime.Parse(dt.ToISOString());
-        }
-
-        if (String.IsNullOrEmpty(m_formatString) == false)
-        {
-          var toStringMethod = value.GetType()
-               .GetMethod("ToString", BindingFlags.Public | BindingFlags.Instance, null, new[] {typeof (string)},
-                          null);
-
-          if (toStringMethod != null)
-          {
-            value = toStringMethod.Invoke(value, new object[] {m_formatString});
-          }
-        }
-
-        context.Write(m_escaped
-            ? Encoders.HtmlEncode(value.ToString())
-            : value.ToString());
+        value = DateTime.Parse(dt.ToISOString());
       }
+
+      if (String.IsNullOrEmpty(m_formatString) == false)
+      {
+        var toStringMethod = value.GetType()
+                                  .GetMethod("ToString", BindingFlags.Public | BindingFlags.Instance, null, new[] {typeof (string)},
+                                             null);
+
+        if (toStringMethod != null)
+        {
+          value = toStringMethod.Invoke(value, new object[] {m_formatString});
+        }
+      }
+
+      context.Write(m_escaped
+                      ? Encoders.HtmlEncode(value.ToString())
+                      : value.ToString());
     }
 
     public override string Source()
