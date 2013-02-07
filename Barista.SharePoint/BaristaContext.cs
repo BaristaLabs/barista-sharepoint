3357cc33033d26a3de2b9a7cf28ee15ff970fd18
Sean McLellan <sean.mclellan@sixconcepts.com>
2013-02-07 16:00:34 +0000

    Code comments.

--- Barista.SharePoint/BaristaContext.cs
+++ Barista.SharePoint/BaristaContext.cs
@@ -9,6 +9,9 @@
     [ThreadStatic]
     private static BaristaContext s_currentContext;
 
+    /// <summary>
+    /// Gets or sets the current Barista Context. If there is no current context one will be created using the current SPContext.
+    /// </summary>
     public static BaristaContext Current
     {
       get
@@ -22,6 +25,9 @@
       set { s_currentContext = value; }
     }
 
+    /// <summary>
+    /// Gets a value that indicates if a current Barista context is available.
+    /// </summary>
     public static bool HasCurrentContext
     {
       get { return s_currentContext != null; }
@@ -31,6 +37,11 @@
     {
     }
 
+    /// <summary>
+    /// Creates a new Barista context using the specified site and web.
+    /// </summary>
+    /// <param name="site"></param>
+    /// <param name="web"></param>
     public BaristaContext(SPSite site, SPWeb web)
     {
       this.Site = site;
@@ -49,53 +60,53 @@
       this.Request = request;
       this.Response = response;
 
-      if (request.ExtendedProperties != null && request.ExtendedProperties.ContainsKey("SPSiteId"))
+      if (request.ExtendedProperties == null || !request.ExtendedProperties.ContainsKey("SPSiteId"))
+        return;
+
+      var siteId = new Guid(request.ExtendedProperties["SPSiteId"]);
+
+      if (siteId != Guid.Empty)
+        this.Site = new SPSite(siteId);
+
+      if (!request.ExtendedProperties.ContainsKey("SPWebId"))
+        return;
+
+      var webId = new Guid(request.ExtendedProperties["SPWebId"]);
+
+      if (webId != Guid.Empty)
+        this.Web = this.Site.OpenWeb(webId);
+
+      if (request.ExtendedProperties.ContainsKey("SPListId"))
       {
-        Guid siteId = new Guid(request.ExtendedProperties["SPSiteId"]);
+        var listId = new Guid(request.ExtendedProperties["SPListId"]);
 
-        if (siteId != Guid.Empty)
-          this.Site = new SPSite(siteId);
+        if (listId != Guid.Empty)
+          this.List = this.Web.Lists[listId];
 
-        if (request.ExtendedProperties.ContainsKey("SPWebId"))
+        if (request.ExtendedProperties.ContainsKey("SPViewId"))
         {
-          Guid webId = new Guid(request.ExtendedProperties["SPWebId"]);
+          var viewId = new Guid(request.ExtendedProperties["SPViewId"]);
 
-          if (webId != Guid.Empty)
-            this.Web = this.Site.OpenWeb(webId);
-
-          if (request.ExtendedProperties.ContainsKey("SPListId"))
-          {
-            Guid listId = new Guid(request.ExtendedProperties["SPListId"]);
-
-            if (listId != Guid.Empty)
-              this.List = this.Web.Lists[listId];
-
-            if (request.ExtendedProperties.ContainsKey("SPViewId"))
-            {
-              Guid viewId = new Guid(request.ExtendedProperties["SPViewId"]);
-
-              if (viewId != Guid.Empty)
-                this.View = this.List.Views[viewId];
-            }
-          }
-
-          if (request.ExtendedProperties.ContainsKey("SPListItemUrl"))
-          {
-            string url = request.ExtendedProperties["SPListItemUrl"];
-
-            if (String.IsNullOrEmpty(url) == false)
-              this.ListItem = this.Web.GetListItem(url);
-          }
-
-          if (request.ExtendedProperties.ContainsKey("SPFileId"))
-          {
-            Guid fileId = new Guid(request.ExtendedProperties["SPFileId"]);
-
-            if (fileId != Guid.Empty)
-              this.File = this.Web.GetFile(fileId);
-          }
+          if (viewId != Guid.Empty)
+            this.View = this.List.Views[viewId];
         }
       }
+
+      if (request.ExtendedProperties.ContainsKey("SPListItemUrl"))
+      {
+        var url = request.ExtendedProperties["SPListItemUrl"];
+
+        if (String.IsNullOrEmpty(url) == false)
+          this.ListItem = this.Web.GetListItem(url);
+      }
+
+      if (!request.ExtendedProperties.ContainsKey("SPFileId"))
+        return;
+
+      var fileId = new Guid(request.ExtendedProperties["SPFileId"]);
+
+      if (fileId != Guid.Empty)
+        this.File = this.Web.GetFile(fileId);
     }
 
     public BrewRequest Request
@@ -146,9 +157,14 @@
       set;
     }
 
+    /// <summary>
+    /// Using the specified SPContext, returns a new Barista Context.
+    /// </summary>
+    /// <param name="context"></param>
+    /// <returns></returns>
     public static BaristaContext CreateContextFromSPContext(SPContext context)
     {
-      BaristaContext result = new BaristaContext();
+      var result = new BaristaContext();
 
       if (context.Site != null)
         result.Site = new SPSite(context.Site.ID);
