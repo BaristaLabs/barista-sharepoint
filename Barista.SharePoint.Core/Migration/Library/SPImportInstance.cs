7f916afb8533ebf7e3f9f8d59a00430793c5226d
Sean McLellan <sean.mclellan@sixconcepts.com>
2013-08-15 02:52:25 +0000

    Fixing Breaking Build; WIP - SPImport

---
+++ Barista.SharePoint.Core/Migration/Library/SPImportInstance.cs
@@ -0,0 +1,49 @@
+﻿namespace Barista.SharePoint.Migration.Library
+{
+  using Barista.Jurassic;
+  using Barista.Jurassic.Library;
+  using System;
+  using Microsoft.SharePoint.Deployment;
+
+  [Serializable]
+  public class SPImportConstructor : ClrFunction
+  {
+    public SPImportConstructor(ScriptEngine engine)
+      : base(engine.Function.InstancePrototype, "SPImport", new SPImportInstance(engine.Object.InstancePrototype))
+    {
+    }
+
+    [JSConstructorFunction]
+    public SPImportInstance Construct()
+    {
+      return new SPImportInstance(this.InstancePrototype);
+    }
+  }
+
+  [Serializable]
+  public class SPImportInstance : ObjectInstance
+  {
+    private readonly SPImport m_import;
+
+    public SPImportInstance(ObjectInstance prototype)
+      : base(prototype)
+    {
+      this.PopulateFields();
+      this.PopulateFunctions();
+    }
+
+    public SPImportInstance(ObjectInstance prototype, SPImport import)
+      : this(prototype)
+    {
+      if (import == null)
+        throw new ArgumentNullException("import");
+
+      m_import = import;
+    }
+
+    public SPImport SPImport
+    {
+      get { return m_import; }
+    }
+  }
+}
---
+++ Barista.SharePoint.Core/Migration/Library/SPImportSettingsInstance.cs
@@ -0,0 +1,49 @@
+﻿namespace Barista.SharePoint.Migration.Library
+{
+  using Barista.Jurassic;
+  using Barista.Jurassic.Library;
+  using System;
+  using Microsoft.SharePoint.Deployment;
+
+  [Serializable]
+  public class SPImportSettingsConstructor : ClrFunction
+  {
+    public SPImportSettingsConstructor(ScriptEngine engine)
+      : base(engine.Function.InstancePrototype, "SPImportSettings", new SPImportSettingsInstance(engine.Object.InstancePrototype))
+    {
+    }
+
+    [JSConstructorFunction]
+    public SPImportSettingsInstance Construct()
+    {
+      return new SPImportSettingsInstance(this.InstancePrototype);
+    }
+  }
+
+  [Serializable]
+  public class SPImportSettingsInstance : ObjectInstance
+  {
+    private readonly SPImportSettings m_importSettings;
+
+    public SPImportSettingsInstance(ObjectInstance prototype)
+      : base(prototype)
+    {
+      this.PopulateFields();
+      this.PopulateFunctions();
+    }
+
+    public SPImportSettingsInstance(ObjectInstance prototype, SPImportSettings importSettings)
+      : this(prototype)
+    {
+      if (importSettings == null)
+        throw new ArgumentNullException("importSettings");
+
+      m_importSettings = importSettings;
+    }
+
+    public SPImportSettings SPImportSettings
+    {
+      get { return m_importSettings; }
+    }
+  }
+}
