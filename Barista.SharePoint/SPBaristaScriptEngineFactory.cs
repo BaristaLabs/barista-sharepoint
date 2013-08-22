b1e03060e9107761bd7a157873a90301992c82b5
Sean McLellan <sean.mclellan@sixconcepts.com>
2013-08-22 19:48:52 +0000

    Adding Sucralose to SP

--- Barista.SharePoint/SPBaristaScriptEngineFactory.cs
+++ Barista.SharePoint/SPBaristaScriptEngineFactory.cs
@@ -60,6 +60,7 @@
 
         instance.Common.RegisterBundle(new StringBundle());
         instance.Common.RegisterBundle(new SugarBundle());
+        instance.Common.RegisterBundle(new SucraloseBundle());
         instance.Common.RegisterBundle(new MomentBundle());
         instance.Common.RegisterBundle(new MustacheBundle());
         instance.Common.RegisterBundle(new LinqBundle());
