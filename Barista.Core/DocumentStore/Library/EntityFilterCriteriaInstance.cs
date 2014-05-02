32ef61e37db51b14e3348ebd7776afad60596bf5
Sean McLellan <sean.mclellan@sixconcepts.com>
2014-05-02 19:16:26 +0000

    Fix for DocumentStore

--- Barista.Core/DocumentStore/Library/EntityFilterCriteriaInstance.cs
+++ Barista.Core/DocumentStore/Library/EntityFilterCriteriaInstance.cs
@@ -1,111 +1,113 @@
 ﻿namespace Barista.Library
 {
-  using Barista.DocumentStore;
-  using Barista.Extensions;
-  using Jurassic;
-  using Jurassic.Library;
-  using Barista.Newtonsoft.Json;
-  using System;
-  using System.Collections.Generic;
+    using Barista.DocumentStore;
+    using Barista.Extensions;
+    using Jurassic;
+    using Jurassic.Library;
+    using Barista.Newtonsoft.Json;
+    using System;
+    using System.Collections.Generic;
 
-  [Serializable]
-  public class EntityFilterCriteriaInstance : ObjectInstance
-  {
-    public EntityFilterCriteriaInstance(ObjectInstance prototype)
-      : base(prototype)
+    [Serializable]
+    public class EntityFilterCriteriaInstance : ObjectInstance
     {
-      this.EntityFilterCriteria = new EntityFilterCriteria();
+        public EntityFilterCriteriaInstance(ObjectInstance prototype)
+            : base(prototype)
+        {
+            this.EntityFilterCriteria = new EntityFilterCriteria();
 
-      this.PopulateFields();
-    }
+            this.PopulateFields();
+        }
 
-    public EntityFilterCriteria EntityFilterCriteria
-    {
-      get;
-      set;
-    }
+        public EntityFilterCriteria EntityFilterCriteria
+        {
+            get;
+            set;
+        }
 
-    [JSProperty(Name = "path")]
-    [JsonProperty("path")]
-    public string Path
-    {
-      get { return EntityFilterCriteria.Path; }
-      set { EntityFilterCriteria.Path = value; }
-    }
+        [JSProperty(Name = "path")]
+        [JsonProperty("path")]
+        public string Path
+        {
+            get { return EntityFilterCriteria.Path; }
+            set { EntityFilterCriteria.Path = value; }
+        }
 
-    [JSProperty(Name = "includeData")]
-    [JsonProperty("includeData")]
-    public bool IncludeData
-    {
-      get { return EntityFilterCriteria.IncludeData; }
-      set { EntityFilterCriteria.IncludeData = value; }
-    }
+        [JSProperty(Name = "includeData")]
+        [JsonProperty("includeData")]
+        public bool IncludeData
+        {
+            get { return EntityFilterCriteria.IncludeData; }
+            set { EntityFilterCriteria.IncludeData = value; }
+        }
 
-    [JSProperty(Name = "namespace")]
-    [JsonProperty("namespace")]
-    public string Namespace
-    {
-      get { return EntityFilterCriteria.Namespace; }
-      set { EntityFilterCriteria.Namespace = value; }
-    }
+        [JSProperty(Name = "namespace")]
+        [JsonProperty("namespace")]
+        public string Namespace
+        {
+            get { return EntityFilterCriteria.Namespace; }
+            set { EntityFilterCriteria.Namespace = value; }
+        }
 
-    [JSProperty(Name = "namespaceMatchType")]
-    [JsonProperty("namespaceMatchType")]
-    public string NamespaceMatchType
-    {
-      get { return EntityFilterCriteria.NamespaceMatchType.ToString(); }
-      set
-      {
-        NamespaceMatchType enumValue;
-        if (value.TryParseEnum(true, out enumValue))
-          EntityFilterCriteria.NamespaceMatchType = enumValue;
-        else
-          throw new JavaScriptException(this.Engine, "Error", "Unknown or invalid namespace match type: " + value);
-      }
-    }
+        [JSProperty(Name = "namespaceMatchType")]
+        [JsonProperty("namespaceMatchType")]
+        public string NamespaceMatchType
+        {
+            get { return EntityFilterCriteria.NamespaceMatchType.ToString(); }
+            set
+            {
+                NamespaceMatchType enumValue;
+                if (value.TryParseEnum(true, out enumValue))
+                    EntityFilterCriteria.NamespaceMatchType = enumValue;
+                else
+                    throw new JavaScriptException(this.Engine, "Error", "Unknown or invalid namespace match type: " + value);
+            }
+        }
 
-    [JSProperty(Name = "queryPairs")]
-    [JsonProperty("queryPairs")]
-    public IDictionary<string, string> QueryPairs
-    {
-      get { return EntityFilterCriteria.QueryPairs; }
-      set { EntityFilterCriteria.QueryPairs = value; }
-    }
+        [JSProperty(Name = "queryPairs")]
+        [JsonProperty("queryPairs")]
+        public IDictionary<string, string> QueryPairs
+        {
+            get { return EntityFilterCriteria.QueryPairs; }
+            set { EntityFilterCriteria.QueryPairs = value; }
+        }
 
-    [JSProperty(Name = "skip")]
-    [JsonProperty("skip")]
-    public int? Skip
-    {
-      get
-      {
-        if (EntityFilterCriteria.Skip != null)
-          return (int)EntityFilterCriteria.Skip;
-        return null;
-      }
-      set
-      {
-        if (value != null)
-          EntityFilterCriteria.Skip = (uint)value;
-        EntityFilterCriteria.Skip = null;
-      }
-    }
+        [JSProperty(Name = "skip")]
+        [JsonProperty("skip")]
+        public int? Skip
+        {
+            get
+            {
+                if (EntityFilterCriteria.Skip != null)
+                    return (int)EntityFilterCriteria.Skip;
+                return null;
+            }
+            set
+            {
+                if (value != null)
+                    EntityFilterCriteria.Skip = (uint)value;
+                else
+                    EntityFilterCriteria.Skip = null;
+            }
+        }
 
-    [JSProperty(Name = "top")]
-    [JsonProperty("top")]
-    public int? Top
-    {
-      get
-      {
-        if (EntityFilterCriteria.Top != null)
-          return (int)EntityFilterCriteria.Top;
-        return null;
-      }
-      set
-      {
-        if (value != null)
-          EntityFilterCriteria.Top = (uint)value;
-        EntityFilterCriteria.Top = null;
-      }
+        [JSProperty(Name = "top")]
+        [JsonProperty("top")]
+        public int? Top
+        {
+            get
+            {
+                if (EntityFilterCriteria.Top != null)
+                    return (int)EntityFilterCriteria.Top;
+                return null;
+            }
+            set
+            {
+                if (value != null)
+                    EntityFilterCriteria.Top = (uint)value;
+                else
+                    EntityFilterCriteria.Top = null;
+            }
+        }
     }
-  }
 }
--- Barista.SharePoint.Core/Library/SPUserInstance.cs
+++ Barista.SharePoint.Core/Library/SPUserInstance.cs
@@ -1,136 +1,136 @@
 ﻿namespace Barista.SharePoint.Library
 {
-  using System;
-  using Jurassic;
-  using Jurassic.Library;
-  using Microsoft.SharePoint;
+    using System;
+    using Jurassic;
+    using Jurassic.Library;
+    using Microsoft.SharePoint;
 
-  [Serializable]
-  public class SPUserConstructor : ClrFunction
-  {
-    public SPUserConstructor(ScriptEngine engine)
-      : base(engine.Function.InstancePrototype, "SPUser", new SPUserInstance(engine.Object.InstancePrototype))
+    [Serializable]
+    public class SPUserConstructor : ClrFunction
     {
+        public SPUserConstructor(ScriptEngine engine)
+            : base(engine.Function.InstancePrototype, "SPUser", new SPUserInstance(engine.Object.InstancePrototype))
+        {
+        }
+
+        [JSConstructorFunction]
+        public SPUserInstance Construct(object arg1)
+        {
+            if (arg1 is SPUserInstance)
+            {
+                return new SPUserInstance(this.Engine.Object.InstancePrototype, (arg1 as SPUserInstance).User);
+            }
+
+            var loginName = TypeConverter.ToString(arg1);
+
+            SPUser user;
+            if (SPHelper.TryGetSPUserFromLoginName(loginName, out user) == false)
+            {
+                throw new JavaScriptException(this.Engine, "Error", "No user with the specified name exists in the current context.");
+            }
+
+            return new SPUserInstance(this.InstancePrototype, user);
+        }
     }
 
-    [JSConstructorFunction]
-    public SPUserInstance Construct(object arg1)
+    [Serializable]
+    public class SPUserInstance : ObjectInstance
     {
-      if (arg1 is SPUserInstance)
-      {
-        return new SPUserInstance(this.Engine.Object.InstancePrototype, (arg1 as SPUserInstance).User);
-      }
+        private readonly SPUser m_user;
 
-      var loginName = TypeConverter.ToString(arg1);
+        public SPUserInstance(ObjectInstance prototype)
+            : base(prototype)
+        {
+            this.PopulateFields();
+            this.PopulateFunctions();
+        }
 
-      SPUser user;
-      if (SPHelper.TryGetSPUserFromLoginName(loginName, out user) == false)
-      {
-        throw new JavaScriptException(this.Engine, "Error", "No user with the specified name exists in the current context.");
-      }
+        public SPUserInstance(ObjectInstance prototype, SPUser user)
+            : this(prototype)
+        {
+            this.m_user = user;
+        }
 
-      return new SPUserInstance(this.InstancePrototype, user);
+        internal SPUser User
+        {
+            get { return m_user; }
+        }
+
+        [JSProperty(Name = "email")]
+        public string Email
+        {
+            get { return m_user.Email; }
+        }
+
+        [JSProperty(Name = "groups")]
+        public SPGroupCollectionInstance Groups
+        {
+            get
+            {
+                return m_user.Groups == null
+                  ? null
+                  : new SPGroupCollectionInstance(this.Engine.Object.InstancePrototype, m_user.Groups);
+            }
+        }
+
+        [JSProperty(Name = "id")]
+        public int Id
+        {
+            get { return m_user.ID; }
+        }
+
+        [JSProperty(Name = "isApplicationPrincipal")]
+        public bool IsApplicationPrincipal
+        {
+            get { return m_user.IsApplicationPrincipal; }
+        }
+
+        [JSProperty(Name = "isDomainGroup")]
+        public bool IsDomainGroup
+        {
+            get { return m_user.IsDomainGroup; }
+        }
+
+        [JSProperty(Name = "isSiteAdmin")]
+        public bool IsSiteAdmin
+        {
+            get { return m_user.IsSiteAdmin; }
+        }
+
+        [JSProperty(Name = "isSiteAuditor")]
+        public bool IsSiteAuditor
+        {
+            get { return m_user.IsSiteAuditor; }
+        }
+
+        [JSProperty(Name = "loginName")]
+        public string LoginName
+        {
+            get { return m_user.LoginName; }
+        }
+
+        [JSProperty(Name = "name")]
+        public string Name
+        {
+            get { return m_user.Name; }
+        }
+
+        [JSProperty(Name = "notes")]
+        public string Notes
+        {
+            get { return m_user.Notes; }
+        }
+
+        [JSProperty(Name = "sid")]
+        public string Sid
+        {
+            get { return m_user.Sid; }
+        }
+
+        [JSFunction(Name = "getXml")]
+        public string GetXml()
+        {
+            return m_user.Xml;
+        }
     }
-  }
-
-  [Serializable]
-  public class SPUserInstance : ObjectInstance
-  {
-    private readonly SPUser m_user;
-
-    public SPUserInstance(ObjectInstance prototype)
-      : base(prototype)
-    {
-      this.PopulateFields();
-      this.PopulateFunctions();
-    }
-
-    public SPUserInstance(ObjectInstance prototype, SPUser user)
-      : this(prototype)
-    {
-      this.m_user = user;
-    }
-
-    internal SPUser User
-    {
-      get { return m_user; }
-    }
-
-    [JSProperty(Name="email")]
-    public string Email
-    {
-      get { return m_user.Email; }
-    }
-
-    [JSProperty(Name = "groups")]
-    public SPGroupCollectionInstance Groups
-    {
-      get
-      {
-        return m_user.Groups == null
-          ? null
-          : new SPGroupCollectionInstance(this.Engine.Object.InstancePrototype, m_user.Groups);
-      }
-    }
-
-    [JSProperty(Name="id")]
-    public int Id
-    {
-      get { return m_user.ID; }
-    }
-
-    [JSProperty(Name = "isApplicationPrincipal")]
-    public bool IsApplicationPrincipal
-    {
-      get { return m_user.IsApplicationPrincipal; }
-    }
-
-    [JSProperty(Name = "isDomainGroup")]
-    public bool IsDomainGroup
-    {
-      get { return m_user.IsDomainGroup; }
-    }
-
-    [JSProperty(Name = "isSiteAdmin")]
-    public bool IsSiteAdmin
-    {
-      get { return m_user.IsSiteAdmin; }
-    }
-
-    [JSProperty(Name = "isSiteAuditor")]
-    public bool IsSiteAuditor
-    {
-      get { return m_user.IsSiteAuditor; }
-    }
-
-    [JSProperty(Name="loginName")]
-    public string LoginName
-    {
-      get { return m_user.LoginName; }
-    }
-
-    [JSProperty(Name = "name")]
-    public string Name
-    {
-      get { return m_user.Name; }
-    }
-
-    [JSProperty(Name="notes")]
-    public string Notes
-    {
-      get { return m_user.Notes; }
-    }
-
-    [JSProperty(Name="sid")]
-    public string Sid
-    {
-      get { return m_user.Sid; }
-    }
-
-    [JSFunction(Name = "getXml")]
-    public string GetXml()
-    {
-      return m_user.Xml;
-    }
-  }
 }
