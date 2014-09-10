f033676939996c80c11d2368823bfcb47b823b5a
Sean McLellan <sean.mclellan@sixconcepts.com>
2014-09-10 19:17:51 +0000

    Added script to remove orphaned barista endpoints.

---
+++ Scripts/RemoveOrphanedBaristaEndpoints.ps1
@@ -0,0 +1,27 @@
+Write-Host
+Write-Host Removing Orphaned IIS Barista Endpoints.
+
+Import-Module "WebAdministration"
+
+#Include the SharePoint cmdlets
+if ((Get-PSSnapin "Microsoft.SharePoint.PowerShell" -ErrorAction SilentlyContinue) -eq $null)
+{
+	Add-PSSnapin "Microsoft.SharePoint.PowerShell" -ErrorAction Inquire -WarningAction Inquire
+} 
+
+$sa = Get-SPServiceApplication | Where { $_.Name -ieq "Barista Service Application" }
+if ($sa -ne $null) {
+    $webApps = Get-WebApplication | Where { $_.PhysicalPath.EndsWith("Barista") -and $_.Path -ne ("/" + $sa.Id.ToString().Substring(0,36).Replace("-",""))}
+
+    if($webApps -ne $null) { 
+        foreach($webApp in $webApps) {
+	        Write-Host "Removing " $webApp.Path
+	        $webAppName = $webApp.path.Replace("/","")
+	        Remove-WebApplication -Site "SharePoint Web Services" -Name $webAppName
+        }
+    }
+    else {
+		Write-Host
+        Write-Host "No Orphan Web Applications Found."
+    }
+}
