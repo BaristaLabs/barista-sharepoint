9331916a1b43cf9c1987dffb1e7e370a4764e9be
Sean McLellan <sean.mclellan@sixconcepts.com>
2014-09-14 07:38:06 +0000

    Service Proxy Name

--- Barista.SharePoint.Core/Services/BaristaServiceProxy.cs
+++ Barista.SharePoint.Core/Services/BaristaServiceProxy.cs
@@ -1,43 +1,44 @@
 ﻿namespace Barista.SharePoint.Services
 {
-  using System;
-  using Microsoft.SharePoint.Administration;
-  using System.Runtime.InteropServices;
+    using System;
+    using Microsoft.SharePoint.Administration;
+    using System.Runtime.InteropServices;
 
-  [Guid("8948B03B-FB70-4B84-897E-A0173C71D819")]
-  [SupportedServiceApplication("9B4C0B5C-8A42-401A-9ACB-42EA6246E960",
-                              "1.0.0.0",
-                              typeof(BaristaServiceApplicationProxy))]
-  public class BaristaServiceProxy : SPIisWebServiceProxy, IServiceProxyAdministration
-  {
-    public BaristaServiceProxy()
+    [Guid("8948B03B-FB70-4B84-897E-A0173C71D819")]
+    [SupportedServiceApplication("9B4C0B5C-8A42-401A-9ACB-42EA6246E960",
+                                "1.0.0.0",
+                                typeof(BaristaServiceApplicationProxy))]
+    public class BaristaServiceProxy : SPIisWebServiceProxy, IServiceProxyAdministration
     {
-    }
+        public BaristaServiceProxy()
+        {
+        }
 
-    public BaristaServiceProxy(SPFarm farm)
-      : base(farm)
-    {
-    }
+        public BaristaServiceProxy(SPFarm farm)
+            : base(farm)
+        {
+            Name = "BaristaServiceProxy";
+        }
 
-    public SPServiceApplicationProxy CreateProxy(Type serviceApplicationProxyType, string name, Uri serviceApplicationUri, SPServiceProvisioningContext provisioningContext)
-    {
-      if (serviceApplicationProxyType != typeof(BaristaServiceApplicationProxy))
-        throw new NotSupportedException();
+        public SPServiceApplicationProxy CreateProxy(Type serviceApplicationProxyType, string name, Uri serviceApplicationUri, SPServiceProvisioningContext provisioningContext)
+        {
+            if (serviceApplicationProxyType != typeof(BaristaServiceApplicationProxy))
+                throw new NotSupportedException();
 
-      return new BaristaServiceApplicationProxy(name, this, serviceApplicationUri);
-    }
+            return new BaristaServiceApplicationProxy(name, this, serviceApplicationUri);
+        }
 
-    public SPPersistedTypeDescription GetProxyTypeDescription(Type serviceApplicationProxyType)
-    {
-      return new SPPersistedTypeDescription("Barista Service Proxy", "Custom service application proxy providing scripted service capabilities.");
-    }
+        public SPPersistedTypeDescription GetProxyTypeDescription(Type serviceApplicationProxyType)
+        {
+            return new SPPersistedTypeDescription("Barista Service Proxy", "Custom service application proxy providing scripted service capabilities.");
+        }
 
-    public Type[] GetProxyTypes()
-    {
-      return new[]
-      {
-        typeof(BaristaServiceApplicationProxy)
-      };
+        public Type[] GetProxyTypes()
+        {
+            return new[]
+            {
+                typeof (BaristaServiceApplicationProxy)
+            };
+        }
     }
-  }
 }
